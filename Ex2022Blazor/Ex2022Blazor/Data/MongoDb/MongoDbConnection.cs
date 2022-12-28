1bcad9e8d214121503aad1c68259b73b756f2927
Vasiliev Roman <46650041+Vamsilver@users.noreply.github.com>
2022-12-28 10:28:39 +0300

    Add files via upload

---
+++ Ex2022Blazor/Ex2022Blazor/Data/MongoDb/MongoDbConnection.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+
+namespace Ex2022Blazor.Data.MongoDb;
+
+public class MongoDbConnection
+{
+    public User? User;
+    public void AddToDatabase(User user)
+    {
+        var client = new MongoClient("mongodb://localhost");
+        var database = client.GetDatabase("Ex2022");
+        var collection = database.GetCollection<User>("Users");
+        collection.InsertOne(user);
+    }
+    
+    public User? FindByEmail(string email)
+    {
+        var client = new MongoClient("mongodb://localhost");
+        FilterDefinition<User> filter = Builders<User>.Filter.Eq("Email", email);
+        var database = client.GetDatabase("Tenders");
+        var collection = database.GetCollection<User>("Users");
+
+        return collection.Find(filter).FirstOrDefault();
+    }
+}
---
+++ Ex2022Blazor/Ex2022Blazor/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex2022Blazor;
+
+public class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public byte[] image { get; set; }
+
+    public User(string firstName, string lastName, string email, string password)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
+    
+}
