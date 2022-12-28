8873ce75bf0bc189b399e32e4a5fc427d779940b
Abramov Danil <112745010+steelbsteel@users.noreply.github.com>
2022-12-28 08:54:07 +0300

    Final

---
+++ Units/Units/BaseClasses/Military.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Units.Units.BaseClasses
+{
+    internal class Military : Unit
+    {
+        public int damage;
+        public int armor;
+
+        public override void Attack()
+        {    
+        }
+    }
+}
---
+++ Units/Units/MilitaryUnits/Tank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Units.Units.BaseClasses;
+
+namespace Units.Units.MilitaryUnits
+{
+    internal class Tank : Military
+    {
+        public bool isBuried;
+        public Tank()
+        {
+            hp = 150;
+            mana = 50;
+            damage = 50;
+        }
+
+        public void Attack()
+        {
+
+        }
+        public void DigIn()
+        {
+            hp += 100;
+        }
+    }
+}
