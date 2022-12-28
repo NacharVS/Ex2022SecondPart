ecc12655509654219b551a20bbcd47263e475343
Dinar <107550834+MardinInc@users.noreply.github.com>
2022-12-28 10:24:31 +0300

    Add files via upload

---
+++ ExamGilaev320/Code/Mongo.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace ExamGilaev320.Code
+{
+    public static class Mongo
+    {
+        public static IMongoDatabase db = new MongoClient("mongodb://localhost").GetDatabase("ExamWorkData");
+
+        public static void AddToDataBase(User user)
+        {
+            var collection = db.GetCollection<User>("ExamWorkData");
+            collection.InsertOne(user);
+        }
+
+        public static User FindByName(string email)
+        {
+            var collection = db.GetCollection<User>("ExamWorkData");
+            var unit = collection.Find(x => x.email == email).FirstOrDefault();
+            return unit;
+        }
+
+        public static IMongoCollection<User> GetCollection()
+        {
+            var filter = new BsonDocument();
+            var collection = db.GetCollection<User>("ExamWorkData");
+
+            return collection;
+        }
+        public static void ReplaceByName(string email, User user)
+        {
+            var filter = new BsonDocument("email", email);
+            var collection = db.GetCollection<User>("ExamWorkData");
+            collection.ReplaceOne(filter, user);
+        }
+
+        internal static User TakeUnit(string? name)
+        {
+            var simpleProjection = Builders<User>.Projection.
+            Exclude("none");
+            BsonClassMap.RegisterClassMap<User>();
+            var filter = Builders<User>.Filter.Eq("name", name);
+            var collection = db.GetCollection<User>("ExamWorkData");
+            return collection.Find(filter).Project<User>(simpleProjection).FirstOrDefault();
+        }
+    }
+}
---
+++ ExamGilaev320/Code/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ExamGilaev320.Code
+{
+    public class User
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId _id;
+        public string email;
+        public string password;
+        public string name;
+        public string surname;
+
+        public User(string name, string surname, string email, string password)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.email = email;
+            this.password = password;
+        }
+    }
+}
