2772c775cdef72fb32ee1895900d2f8d59b1ab1d
Камиль Зиятдинов <k.ziatdinov2003@gmail.com>
2022-12-28 09:25:50 +0300

    Added pages

---
+++ ExamFirst/Exam/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Exam
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExamFirst/ExamTwo/Classes/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamTwo.Classes
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+
+        public Product(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+    }
+}
